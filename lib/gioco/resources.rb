a03feb08e6732312e4c1f2579b5f9f3032e36447
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2012-05-03 16:38:35 -0300

    Whitespace beautification

--- lib/gioco/resources.rb
+++ lib/gioco/resources.rb
@@ -1,14 +1,10 @@
 module Gioco
   class Resources < Core
-
-    def self.change_points( rid, points, resource = false )
-
+    def self.change_points( rid, points, resource = false)
       resource        = get_resource( rid ) if !resource
       new_pontuation  = resource.points.to_i + points
 
-      sync_resource_by_points( resource, new_pontuation )
-
+      sync_resource_by_points(resource, new_pontuation)
     end
-
   end
 end
