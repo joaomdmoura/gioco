fdc475168e05d641939c93d801c5a516b7081f53
Joao Moura <joaomdmoura@gmail.com>
2012-05-07 03:29:24 -0300

    adding some factories

---
+++ spec/factories/badge_fac.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :badge do
+    name    "n00b"
+    points  100
+  end
+end
---
+++ spec/factories/user_fac.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    name    "John Doe"
+    email   "Jonh@doe.com"
+  end
+end
