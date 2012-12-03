85a831f28a1e9459686fb21b6a78a772a9e6a113
Joao Moura <joaomdmoura@gmail.com>
2012-12-03 01:34:26 -0200

    adding new test to seeds support

---
+++ spec/specs/seeds_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe "Gioco: seeds support" do
+  let(:user_a) { FactoryGirl.create(:user) }
+  let(:user_b) { FactoryGirl.create(:user) }
+  let(:user_c) { FactoryGirl.create(:user) }
+
+  context "Running seeds to simulating production environment" do
+
+    before(:all) do
+      User.delete_all
+      Badge.delete_all
+      Type.delete_all
+      Point.delete_all
+    end
+
+    context "Using rake db:seed to recreate all badges registers and relations" do
+    
+      before :all do
+        `cd #{Rails.root}/; rake db:seed`
+      end
+
+      it "All Badges and Types should be created" do
+        Badge.all.size.should == 3
+        Type.all.size.should  == 1
+      end
+    
+    end
+
+  end
+
+end
