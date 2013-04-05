4885fcffca3a117b01586205c91c8c30335ef21b
Joao Moura <joaomdmoura@gmail.com>
2013-04-05 16:14:19 -0300

    adding coveralls to gioco

--- Gemfile
+++ Gemfile
@@ -6,4 +6,5 @@ gem 'rspec-rails',        '2.12.2'
 gem 'rspec',              '2.12.0'
 gem 'rails',              '3.2.12'
 gem 'factory_girl_rails', '4.2.1'
+gem 'coveralls', require: false
 gem 'sqlite3'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 # Configure Rails Envinronmnt
+require 'coveralls'
+Coveralls.wear!
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 Rails.backtrace_cleaner.remove_silencers!
