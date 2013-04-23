37ad811fa5ebca8e6742d7602525dc9b09526080
Joao Moura <joaomdmoura@gmail.com>
2013-04-23 15:44:02 -0300

    refactoring sync_resource_by_points, increasing codeclimate status

--- lib/gioco/core.rb
+++ lib/gioco/core.rb
@@ -4,9 +4,7 @@ class Gioco
       RESOURCE_NAME.capitalize.constantize.find(rid)
     end
 
-    def self.sync_resource_by_points(resource, points, type = false)
-
-      badges = {}
+    def self.related_info(resource, points, type)
       if TYPES && type
         old_pontuation  = resource.points.where(:type_id => type.id).sum(:value)
         related_badges  = Badge.where(((old_pontuation < points) ? "points <= #{points}" : "points > #{points} AND points <= #{old_pontuation}") + " AND type_id = #{type.id}")
@@ -14,8 +12,18 @@ class Gioco
         old_pontuation  = resource.points.to_i
         related_badges  = Badge.where((old_pontuation < points) ? "points <= #{points}" : "points > #{points} AND points <= #{old_pontuation}")
       end
-      
       new_pontuation    = ( old_pontuation < points ) ? points - old_pontuation : - (old_pontuation - points)
+
+      { old_pontuation: old_pontuation, related_badges: related_badges, new_pontuation: new_pontuation }
+    end
+
+    def self.sync_resource_by_points(resource, points, type = false)
+
+      badges         = {}
+      info           = self.related_info(resource, points, type)
+      old_pontuation = info[:old_pontuation]
+      related_badges = info[:related_badges]
+      new_pontuation = info[:new_pontuation]
 
       Badge.transaction do
         if TYPES && type
@@ -39,6 +47,5 @@ class Gioco
         badges
       end
     end
-    
   end
 end
